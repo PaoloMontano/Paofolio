fe7364a64171a7648fc66655ea0400a596709f1f
Paolo Montano <Paolo.Montano@Gmail.com>
2013-06-03 01:25:17 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Hera Films | Vancouver Wedding Filmmakers</title>
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+  <link href="css/style.css" rel="stylesheet" type="text/css" />
+  <link href="css/paofolio.css" rel="stylesheet" type="text/css" />
+  <link href='http://fonts.googleapis.com/css?family=PT+Serif:400,400italic' rel='stylesheet' type='text/css' />
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css' />
+</head>
+<body onload="firstLoad('films.xml')">
+  <?php include('features.php'); ?>
+  <script type="text/javascript" src="js/paofolio.js"></script>
+  
+</body>
+</html>
