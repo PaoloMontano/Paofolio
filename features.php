2ebf4a5d05ea4a79d3e08561f9a564e157dd90bb
Paolo Montano <Paolo.Montano@Gmail.com>
2013-06-03 01:25:41 -0600

    Create features.php

---
+++ features.php
@@ -0,0 +1,28 @@
+<div id="contentContainer" class="contentWrapper">
+  <div id="mediaSection" class="mediaSection">
+    <div id="featureContainer" class="featureContainer">
+      <span id="featuredText" class="featuredText">Featured</span>
+      <p>If you are having difficulties viewing our videos please visit our <a href="#" onclick="return false">Vimeo Page</a></p>
+      <ul>
+        <li>
+          <div id="featureFilmsLink" class="featureLinks">
+            <a href="#" onclick="firstLoad('films.xml'); return false">
+              Films
+            </a>
+          </div>
+        </li>
+        <li>
+          <div id="featurePhotosLink" class="featureLinks">
+            <a href="#" onclick="loadPhotos('photos.xml'); return false">
+              Photos
+            </a>
+          </div>
+        </li>
+      </ul>
+      <div id="mediaScroll" class="mediaScroll">
+        <div id="mediaWrapperID" class="mediaWrapper">
+        </div> <!-- medaWrapperID mediaWrapper -->
+      </div> <!-- mediaScroll mediaScroll -->
+    </div> <!-- featureContainer -->
+  </div> <!-- mediaSection -->
+</div> <!-- contentContainer contentWrapper -->
